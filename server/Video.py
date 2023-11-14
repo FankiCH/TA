84e21619c551a94e636dc7a6e1d0b475bc15ac30
notybre <118180271+notybre@users.noreply.github.com>
2023-11-14 20:52:16 +0500

    Create Video.py

---
+++ server/Video.py
@@ -0,0 +1,13 @@
+class Video():
+    def __init__(self):
+        return
+
+    def get_video(self, video_link):
+        video_data = video_link
+        return video_data
+
+    def save(self):
+        return
+
+video = Video()
+print(video.get_video("link_to_video"))
