0d442cedbbe0ea7ade0d8164c4c96a269774076e
akintev8 <akintev8@gmail.com>
2023-11-18 02:07:09 +0500

    Create Photo.py

---
+++ Photo.py
@@ -0,0 +1,8 @@
+class Photo():
+    def __init__(self):
+        return
+    def get_photo(self,photo_link):
+        photo_data=photo_link
+        return photo_data
+    def save(self):
+        return
